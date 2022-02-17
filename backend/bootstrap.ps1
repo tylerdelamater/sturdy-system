225d2278460a1313dedd259f9bd5447960a50683
Tyler Delamater <tdelamater@dxc.com>
2022-02-17 00:06:12 -0600

    changes to deploy to azure

--- backend/bootstrap.ps1
+++ backend/bootstrap.ps1
@@ -1,4 +1,5 @@
 $env:FLASK_APP = './src/main.py'
 $env:FLASK_ENV = 'development'
+$env:ENV= 'dev'
 c:\Users\tdelamater2\projects\EventFinder\sturdy-system\env\Scripts\activate.ps1
 flask run -h 0.0.0.0
--- src/entities/entity.py
+++ src/entities/entity.py
@@ -3,12 +3,28 @@ from datetime import datetime
 from sqlalchemy import create_engine, Column, String, Integer, DateTime
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+import os
+import urllib
 
-db_url = 'localhost:5432'
-db_name = 'online-exam'
-db_user = 'postgres'
-db_password = '0NLIN3-ex4m'
-engine = create_engine(f'postgresql://{db_user}:{db_password}@{db_url}/{db_name}')
+if os.environ['ENV'] == 'dev':
+    print('using dev db')
+    db_url = 'localhost:5432'
+    db_name = 'online-exam'
+    db_user = 'postgres'
+    db_password = '0NLIN3-ex4m'
+    engine = create_engine(f'postgresql://{db_user}:{db_password}@{db_url}/{db_name}')
+else:
+    #Driver={ODBC Driver 13 for SQL Server};Server=tcp:online-exam.database.windows.net,1433;Database=online-exam;Uid=onlineexamadmin;Pwd={your_password_here};Encrypt=yes;TrustServerCertificate=no;Connection Timeout=30;
+    server = 'online-exam.database.windows.net'
+    database = 'online-exam'
+    username = 'onlineexamadmin'
+    password = "m'3`fs|9ciL?p|E"
+    driver = '{ODBC Driver 13 for SQL Server}'
+    odbc_str = 'DRIVER='+driver+';SERVER='+server+';PORT=1433;UID='+username+';DATABASE='+ database + ';PWD='+ password
+    connect_str = 'mssql+pyodbc:///?odbc_connect=' + urllib.quote_plus(odbc_str)
+    engine = create_engine(connect_str)
+
+
 Session = sessionmaker(bind=engine)
 
 Base = declarative_base()
