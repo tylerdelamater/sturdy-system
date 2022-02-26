eab7b0e868ca11ac1d4ecc586181cb38f59bfdea
Tyler Delamater <tdelamater@dxc.com>
2022-02-25 22:27:12 -0600

    changed port to 80

--- app.py
+++ app.py
@@ -11,4 +11,4 @@ def getmap():
     return jsonify({'id':'1234','name':'Gabba Gabba Hey', 'venue':'Madison Square Garden', 'performer':"Ramones", "when":"16MAY1993"})
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=105)
+    app.run(host='0.0.0.0', port=80)
